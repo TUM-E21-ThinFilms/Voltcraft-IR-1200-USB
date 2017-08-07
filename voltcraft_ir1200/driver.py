7405aa68fae602ed980ca6399b1c9bc314680625
Alexander Book <alexander.book@frm2.tum.de>
2017-08-07 14:04:22 +0200

    updating imports

--- voltcraft_ir1200/driver.py
+++ voltcraft_ir1200/driver.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from protocol import VAT641Protocol
+from protocol import VoltcraftIR1200Protocol
 
 class VoltcraftIR1200Driver(object):
 
